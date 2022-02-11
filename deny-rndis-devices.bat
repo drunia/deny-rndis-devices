2fe25aa4c8e074b7c5ed4cc0ed1b97ef040325aa
drunia <druniax@gmail.com>
2022-02-11 11:02:59 +0200

    little fix

--- deny-rndis-devices.bat
+++ deny-rndis-devices.bat
@@ -16,11 +16,3 @@ for /F %%i in ('reg query %REG_PATHS% ^| findstr /I usb*rndis') do (
 		reg query !REG_PATH!_disabled && reg delete !REG_PATH! /f
 	)
 )
-
-exit /b 0
-
-
-:add_to_sheduler (
-	
-)
-
